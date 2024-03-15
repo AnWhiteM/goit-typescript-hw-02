82d5351680bcec23d7a912fe8af84ad38512e892
Ivan Grivtsov <ivangrivtsov0@gmail.com>
2024-03-15 21:53:42 +0200

    tasks commit

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,10 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
-  return Object.assign(objA, objB);
+function merge<T>(objA: T, objB: T): T {
+  const mergedObject = Object.assign({}, objA, objB);
+  return mergedObject as T;
 }
+
 
 export {}
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,17 +1,21 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+type Props = {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+
+class Component<T extends Props> {
+  constructor(public props: T) {}
+}
+
+
+class Page<T extends Props> extends Component<T> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
+
 
 export {};
